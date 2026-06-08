65242e62096ff9824c501a9c4c91f83c327e449e
pascariu cosmin <pascariucosmin93@gmail.com>
2026-06-08 12:14:43 +0300

    Allow notification service Redis access

--- shared/redis_client.py
+++ shared/redis_client.py
@@ -2,4 +2,9 @@ import redis
 
 from shared.config import settings
 
-redis_client = redis.Redis.from_url(settings.redis_url, decode_responses=True)
+redis_client = redis.Redis.from_url(
+    settings.redis_url,
+    decode_responses=True,
+    socket_connect_timeout=3,
+    socket_timeout=3,
+)
